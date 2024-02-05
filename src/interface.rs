6d1dbcc798c2aad1a5dcf82769d2fa24a34c12f8
Futur Antérieur <futuranterieur7@gmail.com>
2024-02-04 22:14:17 -0500

    Twelfth commit : improving the player's quality of life
    Although there's possibly a more rust-idiomatic way to do what I'm doing there

--- src/interface.rs
+++ src/interface.rs
@@ -51,24 +51,30 @@ impl Interface for CommandLineInterface {
         let sakya_pandita = game_loader::build_game_from_json_string(contents.as_str()).expect("Could not parse game JSON string");
 
         let g = graph::Graph::new(&sakya_pandita);
+        
         let mut current_square_num = sakya_pandita.starting_square;
+        let mut next_square_num = current_square_num.clone();
 
         loop {
-            let current_square_opt = sakya_pandita.squares.iter().find(|square| square.number == current_square_num);
+            let current_square_opt = sakya_pandita.squares.iter().find(|square| square.number == next_square_num);
             if current_square_opt.is_none() {
-                return Err(KarmicCatastrophe{message : format!("You landed on square {}, which does not exist, but may be there when I complete the JSON file.", current_square_num)});
+                println!("You landed on square {}, which does not exist, but may be there when I complete the JSON file.", next_square_num);
+                println!("Going back to where we were...");
+                next_square_num = current_square_num.clone();
+                continue;
             }
-            
+
+            current_square_num = next_square_num.clone();
             let current_square = current_square_opt.unwrap();
             println!("-- {} -- {}", &current_square.number, &current_square.name);
             println!("{}", &current_square.description);
 
-            if current_square_num == sakya_pandita.winning_square {
+            if current_square.number == sakya_pandita.winning_square {
                 println!("This is the end of your journey.");
                 break;
             }
 
-            let mut predecessors = shortest_path::dijkstra(&g, &current_square_num, &sakya_pandita.winning_square, shortest_path::EdgeDistanceMetric::ExpectedRolls).unwrap().predecessors;
+            let mut predecessors = shortest_path::dijkstra(&g, &current_square.number, &sakya_pandita.winning_square, shortest_path::EdgeDistanceMetric::ExpectedRolls).unwrap().predecessors;
             predecessors.reverse();
             let chances_of_reaching_end = g.path_probability(&predecessors);
             println!("Your odds of following the shortest path to the end are {:.7}%.", chances_of_reaching_end*100_f64);
@@ -87,13 +93,13 @@ impl Interface for CommandLineInterface {
             match io::stdin()
             .read_line(&mut choice) {
                 Ok(_size) => (),
-                Err(why) => return Err(KarmicCatastrophe{message : format!("Your answer does please the emptiness : {}", why)})
+                Err(why) => return Err(KarmicCatastrophe{message : format!("Your answer does not please the emptiness : {}", why)})
             }
 
             let true_choice = choice.trim().to_string();
             let maybe_next_node = current_square.paths.get(&true_choice);
             match maybe_next_node {
-                Some(next_node) => current_square_num = next_node.clone(),
+                Some(next_node) => next_square_num = next_node.clone(),
                 None => return Err(KarmicCatastrophe{message : "Your choice lies only in nothingness.".to_string()}),
             }
         }
